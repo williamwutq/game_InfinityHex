b29be78f941520f5a7968964778dfca3dcbed94a
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-06-08 22:01:51 +0800

    Add basic WindowManager

--- engine/HexEngine.cs
+++ engine/HexEngine.cs
@@ -39,10 +39,40 @@ namespace Hex
 
     public class WindowManager{
         private readonly int windowSize;
+        private Block[] blockGrid;
+
+        public delegate void HandleFetchBlock(Hex[] coordinates);
+        public event HandleFetchBlock? OnFetch;
+
         public WindowManager(int windowSize){
             this.windowSize = windowSize;
+            // Calculate array size
+            // Recursive Formula Ak = A(k-1) + 6 * (k-1)
+            // General Formula: Ak = 1 + 3 * (k-1)*(k)
+            this.blockGrid = new Block[1 + 3*(windowSize)*(windowSize-1)];
+            // Add into array to generate the grid
+            int i = 0;
+            for(int a = 1-windowSize; a < windowSize; a ++){
+                for(int b = 1-windowSize; b < windowSize; b ++){
+                    Block nb = new Block(new Hex());
+                    nb.MoveI(b);
+                    nb.MoveK(a);
+                    if(nb.InRange(windowSize)){
+                        blockGrid[i] = nb;
+                        i ++;
+                    }
+                }
+            }
+            // Already sorted by first I then K
+
+            // fetch request
+            OnFetch?.Invoke(Array.ConvertAll(blockGrid, block => block.HexClone()));
         }
 
+        public Block[] GetBlocks() {
+            // This method is not safe. Please do not modify the returned array of Blocks.
+            return this.blockGrid;
+        }
     }
 
     public class HexEngine{
