1cffe555e848e0ff5f46276961dac3751425dbf8
williamwutq <williamwutq@gmail.com>
2025-07-12 22:23:27 +0800

    Finalize graphics in LaunchButton and LaunchContent

--- gui/LaunchButton.cs
+++ gui/LaunchButton.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Layout;
+using Avalonia.Media;
 using System;
 
 namespace game_InfinityHex.UI
@@ -11,18 +12,27 @@ namespace game_InfinityHex.UI
             Background = ThemeManager.DefaultManager.FetchBrush("Launch_Button_Background_Color");
             Foreground = ThemeManager.DefaultManager.FetchBrush("Launch_Button_Text_Color");
             FontFamily = ThemeManager.DefaultManager.FetchFont("Launch_Button_Text_Font");
+            FontStyle = FontStyle.Normal;
+            FontWeight = FontWeight.Bold;
             FontSize = 16;
             HorizontalAlignment = HorizontalAlignment.Center;
             VerticalAlignment = VerticalAlignment.Center;
-            Content = text;
+            Content = new TextBlock
+            {
+                Text = text,
+                TextAlignment = TextAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
             if (action != null) Click += (sender, e) => action();
         }
         public void SetSize(double height)
         {
-            Width = height * 6;
+            Width = height * 3;
             Height = height;
-            FontSize = height / 2;
-            CornerRadius = new Avalonia.CornerRadius(height / 2);
+            FontSize = height * 2 / 3;
+            CornerRadius = new Avalonia.CornerRadius(height / 3);
+            Margin = new Avalonia.Thickness(height / 4, height / 8, height / 4, height / 8);
         }
     }
 }
--- gui/LaunchContent.axaml.cs
+++ gui/LaunchContent.axaml.cs
@@ -7,7 +7,8 @@ namespace game_InfinityHex.UI
 {
     partial class LaunchContent : StackPanel
     {
-        private readonly LaunchButton startButton;
+        private readonly LaunchButton[] buttons;
+        private const double PaddingRatio = 0.15; // 15% total
         public LaunchContent()
         {
             Orientation = Orientation.Vertical;
@@ -15,15 +16,35 @@ namespace game_InfinityHex.UI
             HorizontalAlignment = HorizontalAlignment.Stretch;
             VerticalAlignment = VerticalAlignment.Bottom;
             Spacing = 16;
-            startButton = new LaunchButton("START", null);
+            buttons = [
+                new LaunchButton("START", null),
+            ];
 
-            this.Children.Add(startButton);
+            this.Children.AddRange(buttons);
         }
         public void UpdateLayout(Size containerSize)
         {
+            // Set self size
             Width = containerSize.Width;
-            Height = containerSize.Height / 40 * 33;
+            double expectedHeight = containerSize.Height / 40 * 33;
             Margin = new Thickness(0, 0, 0, containerSize.Height / 20);
+            // Decide children button size
+            double availableSpace = expectedHeight * (1 - PaddingRatio);
+            double spacingCount = buttons.Length + (buttons.Length - 1) * 0.25; // 25% spacing in between buttons
+            double buttonHeight = availableSpace / spacingCount;
+            // Protect against oversize
+            if (buttonHeight > containerSize.Height / 12)
+            {
+                buttonHeight = containerSize.Height / 12;
+            }
+            // Calculate lower padding in addition to the author panel space
+            double totalPadding = (expectedHeight - spacingCount * buttonHeight) / 2;
+            // Calculate the height
+            Height = expectedHeight - totalPadding;
+            foreach (LaunchButton button in buttons)
+            {
+                button.SetSize(buttonHeight);
+            }
         }
     }
 }
