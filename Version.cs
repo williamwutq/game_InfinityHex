ce59067e59de8f051a83e5a270b33dadf1e4a03e
williamwutq <williamwutq@gmail.com>
2025-07-28 20:58:03 -0700

    Bump version

--- Version.cs
+++ Version.cs
@@ -7,7 +7,7 @@ partial class ProgramInfo
     /// <summary>
     /// Gets the current game software version.
     /// </summary>
-    public static System.Version? GameVersion => new(0, 0, 1, 0);
+    public static System.Version? GameVersion => new(0, 0, 2, 0);
     /// <summary>
     /// Gets the name of the game, which is "InfinityHex".
     /// </summary>
